56621d88029dcdd857766e3f4647f3b7e20f457d
nilventosa <ventosa@gmail.com>
2024-12-11 18:49:48 +0100

    empty day 10

---
+++ src/main/java/xyz/ventosa/day10/Day10.java
@@ -0,0 +1,20 @@
+package xyz.ventosa.day10;
+
+import xyz.ventosa.Day;
+
+public class Day10 extends Day  {
+
+    public Day10(String inputFile) {
+        super(inputFile);
+    }
+
+    @Override
+    public Object part1() {
+        return null;
+    }
+
+    @Override
+    public Object part2() {
+        return null;
+    }
+}
